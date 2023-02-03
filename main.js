be31c5aea1e664dfe7c3eae12d3b9f98213c0b24
keita8 <abdulkabirkeita825@gmail.com>
2023-02-03 02:26:45 +0100

    second commit

--- main.js
+++ main.js
@@ -18,7 +18,47 @@ for (let index = 0; index < triggerOpen.length; index++) {
         overlay.classList.add('active');
     });
 
-    targetEl.querySelector('[close-button]').addEventListener('click', openData);
+
+    // targetEl.querySelector('[close-button]').addEventListener('click', openData);
     overlay.addEventListener('click', openData);
+    console.log(overlay);
 
 }
+
+// MOBILE
+// const submenu = document.querySelectorAll('.child-trigger');
+// submenu.forEach((menu) => menu.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('active') : null);
+//     if (this.closest('.has-child').classList != 'active') {
+//         this.closest('.has-child').classList.toggle('active');
+//     }
+
+// }))
+
+
+
+const swiper = new Swiper('.sliderbox', {
+    // Optional parameters
+
+    loop: true,
+    effect: 'fade',
+    autoHeight: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        // disableOnInteraction: true,
+    },
+
+});
