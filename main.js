d78c807b06025e04ca996518a71c385d9cbb07f7
keita8 <abdulkabirkeita825@gmail.com>
2023-02-04 12:38:42 +0100

    changes

--- main.js
+++ main.js
@@ -1,97 +1,139 @@
-const triggerOpen = document.querySelectorAll('[trigger-button]');
-const triggerClose = document.querySelectorAll('[close-button]');
-const overlay = document.querySelector('[data-overlay]');
+const triggerOpen = document.querySelectorAll(['trigger-button']);
+const triggerClose = document.querySelectorAll(['close-button']);
+const overlay = document.querySelector(['data-overlay']);
 
-for (let index = 0; index < triggerOpen.length; index++) {
-    let currentId = triggerOpen[index].dataset.target,
-        targetEl = document.querySelector(`#${currentId}`)
 
-    console.log(targetEl);
+for (let i = 0; i < triggerOpen.length; i++) {
+    let currentId = triggerOpen[i].dataset.target,
+        targetEl = document.querySelector(`#{ currentId }`)
+
     const openData = function() {
         targetEl.classList.remove('active');
         overlay.classList.remove('active');
-        console.log(overlay);
     };
-
-    triggerOpen[index].addEventListener('click', function() {
-        targetEl.classList.add('active');
-        overlay.classList.add('active');
-    });
-
-
-    // targetEl.querySelector('[close-button]').addEventListener('click', openData);
+    triggerOpen[i].addEventListener('click', function() {
+        targetEl.classList.remove('active');
+        overlay.classList.remove('active');
+    })
+    console.log(openData);
+    targetEl.querySelector('[close-button]').addEventListener('click', openData);
     overlay.addEventListener('click', openData);
-    console.log(overlay);
 
 }
 
-// MOBILE
-// const submenu = document.querySelectorAll('.child-trigger');
-// submenu.forEach((menu) => menu.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('active') : null);
-//     if (this.closest('.has-child').classList != 'active') {
-//         this.closest('.has-child').classList.toggle('active');
-//     }
-
-// }))
 
 
 
-const caroussel = new Swiper('.sliderbox', {
+const swiper = new Swiper('.sliderbox', {
     // Optional parameters
 
-    spaceBetween: 30,
-    slidePerView: 'auto',
-    centeredSlides: true,
+    loop: true,
+    effect: 'fade',
+    autoHeight: true,
 
     // If we need pagination
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
     },
 
-    breakpoints: {
-        481: {
-            slidePerView: 2,
-            slidePerGroup: 1,
-            centeredSlides: false
-        },
-        640: {
-            slidePerView: 3,
-            slidePerGroup: 3,
-            centeredSlides: false
-        },
-        992: {
-            slidePerView: 4,
-            slidePerGroup: 4,
-            centeredSlides: false
-        },
+    loop: true,
+    autoplay: {
+        delay: 3000,
     },
 
 });
-// const swiper = new Swiper('.', {
+
+
+
+
+
+//
+
+// const triggerOpen = document.querySelectorAll('[trigger-button]');
+// const triggerClose = document.querySelectorAll('[close-button]');
+// const overlay = document.querySelector('[data-overlay]');
+
+// for (let index = 0; index < triggerOpen.length; index++) {
+//     let currentId = triggerOpen[index].dataset.target,
+//         targetEl = document.querySelector(`#${currentId}`)
+
+//     console.log(targetEl);
+//     const openData = function() {
+//         targetEl.classList.remove('active');
+//         overlay.classList.remove('active');
+//         console.log(overlay);
+//     };
+
+//     triggerOpen[index].addEventListener('click', function() {
+//         targetEl.classList.add('active');
+//         overlay.classList.add('active');
+//     });
+
+
+//     // targetEl.querySelector('[close-button]').addEventListener('click', openData);
+//     overlay.addEventListener('click', openData);
+//     console.log(overlay);
+
+// }
+
+// // MOBILE
+// // const submenu = document.querySelectorAll('.child-trigger');
+// // submenu.forEach((menu) => menu.addEventListener('click', function(e) {
+// //     e.preventDefault();
+// //     submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('active') : null);
+// //     if (this.closest('.has-child').classList != 'active') {
+// //         this.closest('.has-child').classList.toggle('active');
+// //     }
+
+// // }))
+
+
+
+// const caroussel = new Swiper('.sliderbox', {
+//     // Optional parameters
+
+//     spaceBetween: 30,
+//     slidePerView: 'auto',
+//     centeredSlides: true,
+
+//     // If we need pagination
+
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     breakpoints: {
+//         481: {
+//             slidePerView: 2,
+//             slidePerGroup: 1,
+//             centeredSlides: false
+//         },
+//         640: {
+//             slidePerView: 3,
+//             slidePerGroup: 3,
+//             centeredSlides: false
+//         },
+//         992: {
+//             slidePerView: 4,
+//             slidePerGroup: 4,
+//             centeredSlides: false
+//         },
+//     },
+
+// });
+// const swiper = new Swiper('.caroussel', {
 //     // Optional parameters
 
 //     loop: true,
 //     effect: 'fade',
 //     autoHeight: true,
 
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
 
 //     navigation: {
 //         nextEl: '.swiper-button-next',
 //         prevEl: '.swiper-button-prev',
-//     },
-//     loop: true,
-//     autoplay: {
-//         delay: 3000,
-//         // disableOnInteraction: true,
 //     },
 
 // });
